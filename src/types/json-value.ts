29db89d2aaf0bd898637e5a333ceb6cd94f898c6
Brian Westphal <brian.westphal@panchaininc.com>
2022-08-01 11:17:20 +0800

    v1.0.0-alpha.3: - Removed undefined from JsonValue

--- src/types/json-value.ts
+++ src/types/json-value.ts
@@ -2,5 +2,5 @@
 export type JsonArray = JsonValue[];
 /** Any JSON-compatible object value */
 export type JsonObject = { [key: string]: JsonValue };
-/** Any JSON-compatible value.  Including undefined since we serialize it by not-serializing it. */
-export type JsonValue = string | number | boolean | null | JsonArray | JsonObject | undefined;
+/** Any JSON-compatible value */
+export type JsonValue = string | number | boolean | null | JsonArray | JsonObject;
