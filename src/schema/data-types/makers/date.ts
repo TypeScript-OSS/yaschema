346411e6c545ead67c2e3241079d94ec87722b30
Brian Westphal <brian.westphal@panchaininc.com>
2025-03-03 07:28:17 +0800

    v4.4.2 - Fixed date regular expression

--- src/schema/data-types/makers/date.ts
+++ src/schema/data-types/makers/date.ts
@@ -12,7 +12,7 @@ import { validateValueInRange } from '../../internal/utils/validate-value-in-ran
 import type { DateSchema } from '../types/DateSchema';
 
 /** ISO DateTime string */
-const dateRegex = /^\d{4}-\d{2}-\d{2}(T\d{2}:\d{2}(:\d{2}(.\d{1,9})?)?(Z|[+-]\d{2}(:\d{2})?)?)?$/;
+const dateRegex = /^\d{4}-\d{2}-\d{2}(T\d{2}:\d{2}(:\d{2}(\.\d{1,9})?)?(Z|[+-]\d{2}(:\d{2})?)?)?$/;
 
 /** Requires a `Date`, which will be serialized as an ISO Date/Time string */
 export const date = (...allowedRanges: Array<Range<Date>>): DateSchema => new DateSchemaImpl(...allowedRanges);
